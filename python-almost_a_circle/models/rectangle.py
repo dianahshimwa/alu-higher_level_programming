85827f6e697cec9e4ed6f357a462dc2418f2814b
Your Name <d.shimwa@alustudent.com>
2025-08-01 12:45:52 -0700

    Update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,96 +1,88 @@
 #!/usr/bin/python3
-"""Module for Rectangle class."""
+"""Defines the Rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Defines a rectangle, inherits from Base."""
+    """Rectangle class inheriting from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes a Rectangle instance."""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
+    # width getter and setter
     @property
     def width(self):
-        """Gets the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width of the Rectangle."""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        self.validate_integer("width", value)
         self.__width = value
 
+    # height getter and setter
     @property
     def height(self):
-        """Gets the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the height of the Rectangle."""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        self.validate_integer("height", value)
         self.__height = value
 
+    # x getter and setter
     @property
     def x(self):
-        """Gets the x coordinate."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Sets the x coordinate."""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        self.validate_integer("x", value, zero_allowed=True)
         self.__x = value
 
+    # y getter and setter
     @property
     def y(self):
-        """Gets the y coordinate."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Sets the y coordinate."""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        self.validate_integer("y", value, zero_allowed=True)
         self.__y = value
 
+    def validate_integer(self, name, value, zero_allowed=False):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if zero_allowed:
+            if value < 0:
+                raise ValueError(f"{name} must be >= 0")
+        else:
+            if value <= 0:
+                raise ValueError(f"{name} must be > 0")
+
     def area(self):
-        """Returns the area of the Rectangle."""
         return self.width * self.height
 
     def display(self):
-        """Prints the Rectangle using '#' characters."""
         print("\n" * self.y, end="")
         for _ in range(self.height):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-        """Returns a string representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height
-        )
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
-    def update(self, *args):
-        """Assigns arguments to attributes in order:
-        id, width, height, x, y
-        """
-        attrs = ["id", "width", "height", "x", "y"]
-        for i, arg in enumerate(args):
-            if i < len(attrs):
-                setattr(self, attrs[i], arg)
+    def update(self, *args, **kwargs):
+        """Assigns arguments to attributes (in order), or via keyword."""
+        attr_order = ["id", "width", "height", "x", "y"]
+
+        if args and len(args) > 0:
+            for i, value in enumerate(args):
+                if i < len(attr_order):
+                    setattr(self, attr_order[i], value)
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
