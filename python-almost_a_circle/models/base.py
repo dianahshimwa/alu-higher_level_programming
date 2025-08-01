56a5e1100893abc68d11f2cc91aadbd3517acb32
Your Name <d.shimwa@alustudent.com>
2025-08-01 05:36:23 -0700

    IRST CLASS

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines the Base class"""
+
+class Base:
+    """Base class for managing id attribute in future classes."""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base instance.
+        Args:
+            id (int): The identity of the new Base instance.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
